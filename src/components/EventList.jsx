fd38ca4530be0585ff29d82a151d136a3210b6be
ashiyaman <ashiya1005@gmail.com>
2025-02-17 17:47:31 +0530

    feat: Add header component

---
+++ src/components/EventList.jsx
@@ -0,0 +1,12 @@
+const EventList = () => {
+    const eventList = () => {
+
+    }
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default EventList
---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import logo from "../assets/Eventura_Logo.png";
+import { FaFilter } from "react-icons/fa6";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
+const searchHandler = () => {
+  console.log("...in search");
+};
+
+const Header = () => {
+    const [showFilter, setShowFilter] = useState(false)
+
+const toggleFilter = () => {
+    setShowFilter(!showFilter)
+}
+
+  return (
+    <header>
+      <div className="container py-2" style={{backgroundColor: '#d4d2cc'}}>
+        <div className="p-3 d-flex justify-content-between align-items-center">
+            <img src={logo} alt="Eventura Logo" className="img-fluid rounded-circle shadow" style={{maxWidth: '80px'}}/>
+            <div className="d-flex align-items-center position-relative">
+                <button className="border-0 bg-transparent p-2" onClick={toggleFilter}>
+                    <FaFilter className="fs-5" />
+                </button>
+                {showFilter && (
+                <select className="position-absolute mt-2 w-auto border rounded shadow bg-white p-1">
+                    <option>Event Type</option>
+                    <option value="Online">Online</option>
+                    <option value="Offline">Offline</option>
+                    <option value="Both">Both</option>
+                </select>
+                )}
+            </div>            
+        </div>
+        <div className="container d-flex input-group" onClick={searchHandler}>
+            <span className="border-right-0 input-group-text">
+                <FaMagnifyingGlass />
+            </span>
+            <input type="text" placeholder="Search by title and tags" className="form-control border-left-0 input-group-text"/>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
